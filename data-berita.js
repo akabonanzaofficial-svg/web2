3975a5e0681d406310851f9cda613e5d16f6aad6
akabonanzaofficial-svg <akabonanza.official@gmail.com>
2026-04-21 23:37:08 +0700

    Create data-berita.js

---
+++ data-berita.js
@@ -0,0 +1,11 @@
+const dataBerita = [
+    // Berita baru akan di-paste di baris ini nanti
+    { 
+        id: 1713718200000, 
+        judul: "Selamat Datang di Portal Berita SEP", 
+        tanggal: "21 April 2026", 
+        ringkasan: "Ini adalah berita perdana. Sistem CMS Mini Serikat Ekonomi Pesantren telah berhasil diintegrasikan dan siap digunakan.", 
+        gambar: "https://images.unsplash.com/photo-1511632765486-a01980e01a18?w=800&q=80",
+        isiLengkap: "Alhamdulillah, portal berita Serikat Ekonomi Pesantren (SEP) kini telah beroperasi menggunakan sistem CMS mandiri yang ringan dan aman.<br><br>Sistem ini memungkinkan pengurus untuk mempublikasikan kegiatan pesantren secara cepat tanpa memerlukan database server yang rumit. Mari kita wujudkan pesantren yang berdaya, mandiri, dan terintegrasi secara ekonomi untuk kesejahteraan umat!"
+    }
+];
